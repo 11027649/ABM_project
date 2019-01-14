5813248b970238848ccbedb20f353d9cf4422202
11027649 <natasjawezel@hotmail.com>
2019-01-14 15:19:25 +0100

    cleaning

--- SimpleContinuousModule.py
+++ SimpleContinuousModule.py
@@ -1,13 +1,12 @@
 from mesa.visualization.ModularVisualization import VisualizationElement
 
-
 class SimpleCanvas(VisualizationElement):
-    local_includes = ["flockers/simple_continuous_canvas.js"]
+    local_includes = ["simple_continuous_canvas.js"]
     portrayal_method = None
     canvas_height = 500
     canvas_width = 500
 
-    def __init__(self, portrayal_method, canvas_height=500, canvas_width=500):
+    def __init__(self, portrayal_method, canvas_height=750, canvas_width=750):
         '''
         Instantiate a new SimpleCanvas
         '''
--- Test_Steven/run.py
+++
@@ -1,5 +0,0 @@
-from server import server
-
-# launch at default port
-server.port = 8521
-server.launch()
