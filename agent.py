49cee78eeeef29fec8392465fbd8d87efe6a7fe8
Steven <steven17@live.nl>
2019-01-15 01:39:53 +0100

    Late night-crazy if-statements for no collision

--- agent.py
+++ agent.py
@@ -38,10 +38,12 @@ class Pedestrian(Agent):
         '''
 
         # check if there's a traffic light (and adjust speed accordingly)
+        changed = 0
         for i in self.model.space.get_neighbors(self.pos, include_center = False, radius = 4):            
-            if isinstance(i,Light) and i.state < 50:
+            if self.check_front() > 0 or isinstance(i,Light) and i.state < 50:
                 self.speed = 0
-            elif isinstance(i, Light) and i.state >= 50:
+                changed = 1
+            elif changed == 0 and self.check_front() == 0 or (isinstance(i, Light) and i.state >= 50 and self.check_front() == 0):
                 self.speed = 1
 
         # take a step
@@ -51,6 +53,24 @@ class Pedestrian(Agent):
         else:
             next_pos = (self.pos[0], self.pos[1] - self.speed)
             self.model.space.move_agent(self, next_pos)
+
+    # this function is in both pedestrian and agent -> more efficient way?
+    def check_front(self):
+        '''
+        Function to see if there is a car closeby in front of a car
+        '''
+
+        if self.dir == "up":
+            direction = 1
+        else:
+            direction = -1
+
+        # the car has a vision range of 1 tile for now (can be changed to its max speed?)
+        for i in range(1, 2):
+            for agent in self.model.space.get_neighbors((self.pos[0], self.pos[1] + direction * i), radius = 0):
+                if isinstance(agent, Car) or isinstance(agent, Pedestrian):
+                    return i
+        return 0
 
 
 class Car(Agent):
@@ -65,13 +85,14 @@ class Car(Agent):
         '''
         Cars go straight for now.
         '''
-
+        changed = 0
         for i in self.model.space.get_neighbors(self.pos, include_center = False, radius = 2):
         # not only affected by 1 light
-            if isinstance(i,Light) and i.state > 50:
+            if self.check_front() > 0 or isinstance(i,Light) and i.state > 50:
                 # if self.speed > 0:
                 self.speed = 0
-            elif isinstance(i, Light) and i.state <= 50:
+                changed = 1
+            elif changed == 0 and self.check_front() == 0 or (isinstance(i, Light) and i.state <= 50 and self.check_front() == 0):
                 # if self.speed < 1:
                 self.speed = 1
 
@@ -82,3 +103,20 @@ class Car(Agent):
         else:
             next_pos = (self.pos[0] + self.speed, self.pos[1])
             self.model.space.move_agent(self, next_pos)
+    
+    def check_front(self):
+        '''
+        Function to see if there is a car closeby in front of a car
+        '''
+
+        if self.dir == "right":
+            direction = 1
+        else:
+            direction = -1
+
+        # the car has a vision range of 1 tile for now (can be changed to its max speed?)
+        for i in range(1, 2):
+            for agent in self.model.space.get_neighbors((self.pos[0] + direction * i, self.pos[1]), radius = 0):
+                if isinstance(agent, Car) or isinstance(agent, Pedestrian):
+                    return i
+        return 0
