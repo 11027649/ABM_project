b33ad7d58c059a69d632877fc405ed0421f002dd
Steven <steven17@live.nl>
2019-01-30 16:31:18 +0100

    merge shit

--- model.py
+++ model.py
@@ -150,7 +150,7 @@ class Traffic(Model):
                 if self.space.get_neighbors((pos[0] + 5 * i, pos[1]), include_center = True, radius = 2.5):
                     car_near = True
 
-            if random.random() < 0.02 and car_near == False:
+            if random.random() < 0.05 and car_near == False:
                 self.new_car(pos, "right")
 
         else:
@@ -161,7 +161,7 @@ class Traffic(Model):
             for i in range(5):
                 if self.space.get_neighbors((pos[0] - 5 * i, pos[1]), include_center = True, radius = 2.5):
                     car_near = True
-            if random.random() < 0.02 and car_near == False:
+            if random.random() < 0.05 and car_near == False:
                 self.new_car(pos, "left")
 
 
@@ -171,14 +171,14 @@ class Traffic(Model):
             pos = (self.x_max / 2 - 1 , self.y_max - 1)
         #     pos = (random.uniform(24*2,26*2),  self.y_max - 1)
 
-            if random.random() < 0.06 and not self.space.get_neighbors(pos, include_center = True, radius = 0.8):
+            if random.random() < 0.05 and not self.space.get_neighbors(pos, include_center = True, radius = 0.8):
                 self.new_pedestrian(pos, "up")
 
         # else:
             pos = (self.x_max / 2 + 1, 0)
         #     pos = (random.uniform(24*2,26*2),  0)
 
-            if random.random() < 0.06 and not self.space.get_neighbors(pos, include_center = True, radius = 0.8):
+            if random.random() < 0.05 and not self.space.get_neighbors(pos, include_center = True, radius = 0.8):
                 self.new_pedestrian(pos, "down")
 
         # save the statistics
